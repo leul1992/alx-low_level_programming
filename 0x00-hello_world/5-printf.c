59e785160020e0e4e2f3a1ecd1f6cceda1cd96a4
leul1992 <leulbekele191@gmail.com>
2022-03-10 18:17:21 +0000

    prints output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-
 int main(void)
 {
-     printf("%s\n","with proper grammar, but the outcome is a piece of art,");
-     return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
