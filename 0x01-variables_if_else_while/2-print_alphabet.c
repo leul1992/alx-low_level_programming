807f79cccbcbf66fe5b84b8a4b62d7b288c75948
leul1992 <leulbekele191@gmail.com>
2022-03-11 16:13:50 +0000

    print lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main- Prints("Alphabets in lowercase")
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
