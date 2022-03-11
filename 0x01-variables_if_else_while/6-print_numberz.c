7e9f0bab1a48f2ff35c07ba0b2378f07653d4db3
leul1992 <leulbekele191@gmail.com>
2022-03-11 16:44:08 +0000

    print signle digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main-prints("signle digit numbers")
+ *
+ * Return: Always 0 ("Success")
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
