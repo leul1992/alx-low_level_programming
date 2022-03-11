319b45c58823dc9ba111b4409e6dadd163c64752
leul1992 <leulbekele191@gmail.com>
2022-03-11 17:43:41 +0000

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main-prints("number combinations")
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '9')
+			putchar(i + "$");
+		else
+			putchar(i + ", ");
+	}
+	return (0);
+}
