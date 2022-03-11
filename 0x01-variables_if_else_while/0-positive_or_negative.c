16414f4c07509effeb105d974722c6d75533a28e
leul1992 <leulbekele191@gmail.com>
2022-03-11 15:52:17 +0000

    writting c code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio>
+/**
+ * main- Prints("positve zero negative")
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		prints("is positive");
+	else if (n == 0)
+		prints("is zero");
+	else
+		prints("is negative");
+	return (0);
+}
