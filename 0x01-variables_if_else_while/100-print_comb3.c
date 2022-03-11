47b8cf77c2e8e410bcc661a80ad5fe7d7b75b9ad
leul1992 <leulbekele191@gmail.com>
2022-03-11 18:09:52 +0000

    two digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main-prints("two digit combinations")
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numx;
+	int numy;
+
+	for (numx = '0'; numx <= '9'; numx++)
+		for (numy = '0'; numy <= '9'; numy++)
+		{
+			putchar(numx);
+			putchar(numy);
+			if (numx != 9 && numy != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
