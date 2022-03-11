8b29026024662bf896952bf1fbf3f715da885c4d
leul1992 <leulbekele191@gmail.com>
2022-03-11 16:38:35 +0000

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main- prints("single digit numbers")
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		printf(i);
+	putchar(10);
+	return (0);
+}
