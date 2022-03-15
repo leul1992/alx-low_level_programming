f1e08a2f02b1e3d760118f35e664f0396c1356f0
leul1992 <leulbekele191@gmail.com>
2022-03-15 17:51:12 +0000

    checks sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * int print_sign(int);
+ * @n: check if it is a sign number
+ * Return: (1) if it is greater than 0
+ * (0) if it is zero, (-1) if it is less than 0
+ */
+int print_sign(int n)
+{
+	int ch;
+
+	if (n > 0)
+	{
+		ch = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		ch = 0;
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		ch = -1;
+		_putchar('-');
+	}
+}
