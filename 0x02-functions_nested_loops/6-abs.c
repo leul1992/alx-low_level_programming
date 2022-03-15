b7550ba01bbed73629498a4bbf812669eedccf9b
leul1992 <leulbekele191@gmail.com>
2022-03-15 18:57:49 +0000

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include"main.c"
+/**
+ * _abs-"compute the absolute value"
+ * @n: number to compute
+ * Return: the absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else if (n < 0)
+		return (n * -1);
+}
