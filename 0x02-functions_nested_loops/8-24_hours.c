0b69a939323cca4ed0f2aa7da1d2231d3354d69b
leul1992 <leulbekele191@gmail.com>
2022-03-15 20:51:46 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include"main.h"
+/**
+ * jack_bauer-"Print every minute of a day"
+ * Return: every minute of a day
+ */
+void jack_bauer(void)
+{
+	int h1 = 0;
+	int h2 = 0;
+	int m1 = 0;
+	int m2 = 0;
+
+	while (h1 < 3)
+	{
+		int ch = 10;
+
+		if (h1 == 2)
+			ch = 4;
+		while (h2 < ch)
+		{
+			while (m1 < 60)
+			{
+				while (m2 < 10)
+				{
+				_putchar('0' + h1);
+				_putchar('0' + h2);
+				_putchar(':');
+				_putchar('0' + m1);
+				_putchar('0' + m2);
+				-putchar('\n');
+				m2++;
+				}
+				m1++;
+			}
+			h2++;
+		}
+		h1++;
+	}
+}
