004fdbc3798b5772dc798dd869bddaf8cd4c7ec6
leul1992 <leulbekele191@gmail.com>
2022-03-15 22:27:17 +0000

    print upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,16 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 		_putchar('0' + n);
 		n++;
 	}
-	while (n >= 98)
+	while (n > 98)
 	{
 		_putchar('0' + n);
 		n--;
 	}
+	_putchar('98');
 	_putchar('\n');
 }
