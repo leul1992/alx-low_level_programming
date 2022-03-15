fe832848307fe871520fcfb744c1ffb84e5caf05
leul1992 <leulbekele191@gmail.com>
2022-03-15 16:40:41 +0000

    first function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main-"prints word"
+ * Return: (0)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (i < str.length())
+	{
+	_putchar(str[i]);
+	i++
+	}
+	_putchar('\n');
+	return (0);
+}
