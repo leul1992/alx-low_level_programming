243f146f608a2f984807bd255a743dd283f96def
leul1992 <leulbekele191@gmail.com>
2022-03-16 08:07:28 +0000

    the first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main-"prints fibonacci"
+ * Return: (0)
+ */
+int main(void)
+{
+	int prev = 0;
+	int curr = 1;
+	int count = 0;
+	int temp;
+
+	while (count < 50)
+	{
+		printf("%d, ,",prev + curr);
+		temp = curr;
+		curr = prev + curr;
+		prev = temp;
+		count ++;
+	}
+	return (0);
+}
