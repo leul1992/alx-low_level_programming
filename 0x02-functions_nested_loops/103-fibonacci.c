be2dc07872a23423ac0382221507c6ff5107803e
leul1992 <leulbekele191@gmail.com>
2022-03-16 11:45:54 +0000

    fibonacci till 4mil

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,22 +5,19 @@
  */
 int main(void)
 {
-	long int prev = 0;
-	long int curr = 1;
-	long int max;
-	long int temp;
+	int prev = 0;
+	int curr = 1;
+	int max;
+	int sum = 0;
 
 	while (max < 4000000)
 	{
-		if (max + curr < 4000000)
-			printf("%lu", max);
-		else
-			printf("%lu, ", max);
-		temp = curr;
-		curr = prev + curr;
-		prev = temp;
 		max = prev + curr;
+		prev = curr;
+		curr = max;
+		if (max % 2 == 0)
+			sum += max;
 	}
-	printf("\n");
+	printf("%i\n", sum);
 	return (0);
 }
