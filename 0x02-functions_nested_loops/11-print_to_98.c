b8e471099e681cec2817aa995244a5f748e0a8a3
leul1992 <leulbekele191@gmail.com>
2022-03-16 07:23:30 +0000

    print upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,22 @@
 #include"main.h"
+#include<stdio.h>
 /**
- * print_to_98 - print to 98
+ * print_to_98 - "print to 98"
  * @n : number to start from
  * Return:0 or 1
  */
-
 void print_to_98(int n)
 {
-
 	while (n < 98)
 	{
-		_putchar('0' + n);
-		_putchar(',');
-		_putchar(' ');
+		printf("%i, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		_putchar('0' + n);
-		_putchar(',');
-		_putchar(' ');
+		printf("%i, ", n);
 		n--;
 	}
-	_putchar('0' + n);
-	_putchar('\n');
+	printf("98");
+	putchar('\n');
 }
