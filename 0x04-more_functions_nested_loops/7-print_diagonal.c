459ec2c4ab68fa7cb174bb49a0d1f0edcfafc4ea
leul1992 <leulbekele191@gmail.com>
2022-03-17 13:57:51 +0000

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal-"draw a diagonal line"
+ * @n:number of backward slash
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('$');
+		}
+	}
+	else
+		_putchar('$');
+	_putchar('\n');
+}
