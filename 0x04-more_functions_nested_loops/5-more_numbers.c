22d3d401295fe44204a6fe38c190c6da66f426f7
leul1992 <leulbekele191@gmail.com>
2022-03-17 13:13:17 +0000

    print 0-14 10X

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers-"print 0-14 10X"
+ * Return: (0)
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			_putchar('0' + j);
+			j++;
+		}
+		i++;
+	}
+}
