09bfcbd5d2d15456d9b54313ac6e7360c5363a49
leul1992 <luelbekele191@gmail.com>
2022-03-18 11:54:09 +0000

    get primefactor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main-"print the prime factor of a number"
+ * Return: (0)
+ */
+int main(void)
+{
+	long long int num = 612852475143;
+	int n;
+
+	n = 2;
+	while (num != 1)
+	{
+		if (num % n == 0)
+			num = num / n;
+		else
+			n++;
+	}
+	printf("%i\n", n);
+	return (0);
+}
