d8ca5470988a3ece12290b123d918c85a08ebd7c
leul1992 <luelbekele191@gmail.com>
2022-03-18 14:38:46 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,27 +6,30 @@
  */
 void print_number(int n)
 {
-	int i;
-	int tmp;
-	int ic;
+	int d = 1, i = 0, itmp = 0;
 
-	i = 10;
 	if (n < 0)
 	{
-		ic = n;
-		n = -1 * n;
-	}
-	while (n / i != 0)
-		i = i * 10;
-	if (ic < 0)
 		_putchar('-');
-	while (n != 0)
-	{
-		i = i / 10;
-		tmp = n / i;
-		_putchar('0' + tmp);
-		n = n % i;
+		n = -n;
 	}
 
+	while (n / d != 0)
+	{
+		d *= 10;
+		i++;
+	}
+	d = d / 10;
+
+	while (itmp < i)
+	{
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		itmp++;
+	}
+
+	if (i == 0)
+		_putchar('0' + n);
 
 }
