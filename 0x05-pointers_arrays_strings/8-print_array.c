14a50c367e6c8b8a46c5cdf84d3c57329741b358
leul1992 <luelbekele191@gmail.com>
2022-03-21 20:26:41 +0000

    print number of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * print_array-"print array of integers"
+ * @a: the array
+ * @n: number of elements
+ * Return:(0)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%i", a[i]);
+		else
+			printf("%i, ", a[i]);
+	}
+}
