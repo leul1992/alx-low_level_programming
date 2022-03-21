08102d500d01d7510d575e163879d72e5dd1aadb
leul1992 <luelbekele191@gmail.com>
2022-03-21 20:40:03 +0000

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy-"copy a string"
+ * @dest:copy here
+ * @src:copy this
+ * Return: (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
