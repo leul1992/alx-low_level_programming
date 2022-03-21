aeb9e40a78cc545926be73de410ec74b81e98ce3
leul1992 <luelbekele191@gmail.com>
2022-03-21 19:02:34 +0000

    print string, print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_rev-"print string in reverse"
+ * @s:the string to print
+ * Return:(0)
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	for (; i >= 0; i--)
+		putchar(s[i]);
+}
