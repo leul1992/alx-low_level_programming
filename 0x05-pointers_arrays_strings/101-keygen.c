973a2e70a69e7ac9e18b4e3665e97bbb11045536
leul1992 <luelbekele191@gmail.com>
2022-03-22 19:30:10 +0000

    random pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main-generate random valid pass
+ * Return:(0)
+ */
+int main(void)
+{
+	int i;
+	int sum;
+	int j;
+	int pass[100];
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2772 - sum - '0';
+			sum += j;
+			putchar(j + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
