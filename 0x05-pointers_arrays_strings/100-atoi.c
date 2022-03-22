63a899a0b4b52a04cb58f5d95b8e15c726c81ae3
leul1992 <luelbekele191@gmail.com>
2022-03-22 19:02:19 +0000

    string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,10 +8,12 @@ int _atoi(char *s)
 	int i;
 	int sign;
 	int num;
+	int bre;
 
 	i = 0;
 	num = 0;
 	sign = -1;
+	bre = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
@@ -20,7 +22,10 @@ int _atoi(char *s)
 		{
 			num = num * 10;
 			num -= s[i] - '0';
+			bre = 1;
 		}
+		else if (bre == 1)
+			break;
 		i++;
 	}
 	num = num * sign;
