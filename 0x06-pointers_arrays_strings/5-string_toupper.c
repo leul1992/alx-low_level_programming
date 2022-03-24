f456b8768de7553fcdffc3c04d4fe62bbd248977
leul1992 <luelbekele191@gmail.com>
2022-03-24 17:28:10 +0000

    encode string,capitalize string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,29 @@
-#include "
+#include "main.h"
+/**
+ * string_toupper-"changes all lower case to uppercase"
+ * @c:the characters
+ * Return: the uppercase of the characters
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+	char j, k;
+
+	while (c[i] != '\0')
+	{
+		k = 'A';
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			for (j = 'a'; j <= 'z'; j++)
+			{
+				if (c[i] == j)
+					c[i] = k;
+				k++;
+			}
+
+		}
+		i++;
+	}
+	i--;
+	return (c);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * cap_string-"to uppercase "
+ * @c:the characters
+ * Return:uppercased characters
+ */
+char *cap_string(char *)
+{
+
+}
