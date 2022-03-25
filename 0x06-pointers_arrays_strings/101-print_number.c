4612926c9c1c66b10c2155331348b2b52a37a8e9
leul1992 <luelbekele191@gmail.com>
2022-03-25 17:45:20 +0000

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,25 +6,19 @@
  */
 void print_number(int n)
 {
-	unsigned int i = 1;
+	unsigned int n1;
 
-	if (n == 0)
-		_putchar('0' + n);
+	n1 = n;
+
 	if (n < 0)
 	{
-		n = n * -1;
 		_putchar('-');
-	}
-	while (n / i != 0)
-	{
-		i = i * 10;
-	}
-	i = i / 10;
-	while (n != 0)
-	{
-		_putchar('0' + n / i);
-		n = n % i;
-		i = i / 10;
+		n1 = -n;
 	}
 
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * infinite_add-"add two numbers"
+ * @n1:number one
+ * @n2:number two
+ * @r:buffer
+ * @size_r:buffer size
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, temp;
+
+	while (i != size_r || !( n1[i] == '\0' && n2[i] == '\0'))
+	{
+		temp = (n1 - '0') + (n2 - '0');
+		r[i] = temp + '0';
+		i++;
+	}
+}
