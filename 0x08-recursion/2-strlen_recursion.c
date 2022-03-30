ab4cc1140fdecdb2a8269d476e1d7f1d04f46423
leul1992 <luelbekele191@gmail.com>
2022-03-30 11:46:37 +0000

    return string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count = 1;
+	int count = 0;
 
-	if (*s == '\0')
-		return (0);
-	else
+	if (*s > '\0')
 	{
-		return (count + _strlen_recursion(s + 1));
+		count += _strlen_recursion(s + 1) + 1;
 	}
+	return (count);
 }
