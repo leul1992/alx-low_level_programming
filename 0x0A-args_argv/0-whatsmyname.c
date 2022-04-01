a8e64900c78984c7a102d24efa984990cfef9ced
leul1992 <luelbekele191@gmail.com>
2022-04-01 18:25:19 +0000

    print programs name

---
+++ 0x0A-args_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-print its name
+ * @argc:argument count
+ * @argv:argument of array pointer to char
+ * Return (0);
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
