2dec4bb96709bec56747d921db71ae5e5e294d1d
leul1992 <luelbekele191@gmail.com>
2022-04-01 19:39:32 +0000

    number of arguments passed to it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main-"print the number of arguments passed into it"
- * @argc:number of arguments(int)
- * @argv:pointer to argument array
- * Return : (0)
+ * main - prints the number of arguments passed into the program
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 0
  */
 
 int main(int argc, char const *argv[])
