4e403845869b273fa53176aa6313e0b018059dc4
leul1992 <luelbekele191@gmail.com>
2022-04-01 19:43:22 +0000

    print multiplication of two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main-multiply numbers
- * @argc:argument number
- * @argv:list of argument
+ * main - multiply numbers
+ * @argc : argument number
+ * @argv : list of argument
  * Return : (0)
  */
-int main(int argc, char const **argv)
+
+int main(int argc, char const *argv[])
 {
+	(void)argc;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (0);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
