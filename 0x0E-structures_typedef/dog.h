d289a634d774bc44901efcaed2c11ed29562ceb3
leul1992 <luelbekele191@gmail.com>
2022-04-11 19:39:08 +0000

    difine structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 typedef struct dog
 {
 	char *name;
