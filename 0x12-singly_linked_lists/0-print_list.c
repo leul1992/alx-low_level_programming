a8a1cb08d88f88eb38a232121f18a8aec2187100
Leulseged <leulbekele191@gmail.com>
2022-04-21 23:22:39 +0300

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list-prints all the elements
+ * @h:pointer to the lists
+ * Return:number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t start = malloc(sizeof(list_t);
+
+	list_t.len = 0;
+
+	while (start != NULL)
+	{
+		printf("%s", start);
+		start = start->next;
+		list_t.len++;
+	}
+	printf("\n");
+}
