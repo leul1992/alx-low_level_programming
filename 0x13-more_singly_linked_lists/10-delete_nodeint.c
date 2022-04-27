502c6f83f4e1869146cba65ee9040240b4ce5ac6
Leulseged <leulbekele191@gmail.com>
2022-04-27 21:42:24 +0300

    delte node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,45 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * delete_nodeint_at_index-deletes node at a position
- * @head:the head of the list
- * @index:the position to be deleted
- * Return: 1(success) or 0(failure)
+ * delete_nodeint_at_index - deletes a node at a given position.
+ * @head: head of linked list
+ * @index: index to delete
+ * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *cur, *curprev;
+	listint_t *current;
+	listint_t *tmp;
 
-	curprev = *head;
-	while (i != index - 1)
+	if (head == NULL || (*head) == NULL)
+		return (-1);
+
+	current = *head;
+	if (index == 0)
 	{
-		if (curprev == NULL)
-		{
-			return (0);
-		}
-		curprev = curprev->next;
-		i++;
-	}
-	cur = curprev->next;
-	if (cur == NULL)
-	{
-		return (0);
-	}
-	if (cur->next == NULL)
-	{
-		curprev->next = NULL;
+		if ((*head)->next)
+			(*head) = (*head)->next;
+		else
+			(*head) = NULL;
+		free(current);
 		return (1);
 	}
+
+	while (index != 1)
+	{
+		if (current->next == NULL)
+			return (-1);
+
+		current = current->next;
+		--index;
+	}
+	tmp = current->next;
+	if (current->next->next)
+		current->next = current->next->next;
 	else
-	{
-		curprev->next = cur->next;
-		return (1);
-	}
+		current->next = NULL;
+	free(tmp);
+
+	return (1);
 }
