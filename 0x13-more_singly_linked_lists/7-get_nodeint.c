1e868a216261a67ce45cd74b28e38e7a595f63cc
Leulseged <leulbekele191@gmail.com>
2022-04-27 21:57:03 +0300

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 
 	current = head;
+	if (head == NULL)
+		return (0);
 	while (i != index)
 	{
 		current = current->next;
