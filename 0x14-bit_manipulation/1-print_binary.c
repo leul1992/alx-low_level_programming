af1b44d395cfc3ffdcc6a83241e3e3e319ec1543
Leulseged <leulbekele191@gmail.com>
2022-04-28 22:24:06 +0300

    return bit at a given index

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+/**
+ * print_binary-prints the binary represenation of a number
+ * @n: the number
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	char *c;
+
+	while (n > 0)
+	{
+
+	}
+}
