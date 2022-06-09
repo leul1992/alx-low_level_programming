01c1ac4544fee2badd9ddf3614e5c7bebc473263
Leulseged <leulbekele191@gmail.com>
2022-06-09 21:56:56 +0300

    add node at end error fix, insert at a given postion

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,14 +10,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
+
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
+
 	return (new);
 }
