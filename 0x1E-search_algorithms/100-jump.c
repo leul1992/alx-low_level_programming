b4af1307588d55da6b6c9e29b98300f2a6869da6
Leulseged <leulbekele191@gmail.com>
2022-10-04 17:46:39 +0300

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * jump_search- searches for a value in jump search
+ * @array: the pointer to the array to search in
+ * @size: the size of the array
+ * @value: the value to search for
+ * Return: index of the value(success) or -1(failure)
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t m = (int)floor(sqrt(size)), index = 0, i;
+
+	while (array[index] < value && array && index < size)
+	{
+		printf("Value checked array [%ld] = [%ld]\n", index, index);
+		index += m;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", index - m, index);
+	i = index - m;
+	while (i <= index && i < size)
+	{
+		printf("Value checked array [%ld] = [%ld]\n", i, i);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
