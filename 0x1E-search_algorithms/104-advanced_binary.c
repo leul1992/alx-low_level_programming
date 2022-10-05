46cedea3ead360751c9761cb8753bfee84e27810
Leulseged <leulbekele191@gmail.com>
2022-10-05 14:15:40 +0300

    advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -13,7 +13,7 @@ int advanced_binary(int *array, size_t size, int value)
 
 	if (!array)
 		return (-1);
-	mid = floor((first + last) / 2);
+	mid = floor((first + last - 1) / 2);
 	printf("Searching in array: \n");
 	for (i = first; i < last; i++)
 	{
